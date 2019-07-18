5182b5f5087e054f86261737128010939a5332c4
Christopher Kim <chrisms91@gmail.com>
2019-07-17 23:45:26 -0700

    fetching weather info

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
-import Weather from "./Weather";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Weather from './Weather';
+
+const API_KEY = '8042367a40d903407a2eee2c0cfa759a';
 
 export default class App extends Component {
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null
   };
-
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+  _getWeather = (lat, lon) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         {isLoaded ? (
@@ -16,6 +41,7 @@ export default class App extends Component {
         ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -26,16 +52,21 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: '#fff'
   },
   loading: {
     flex: 1,
-    backgroundColor: "#FDF6AA",
-    justifyContent: "flex-end",
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
     paddingLeft: 25
   },
   loadingText: {
     fontSize: 38,
-    marginBottom: 100
+    marginBottom: 60
+  },
+  errorText: {
+    color: 'red',
+    backgroundColor: 'transparent',
+    marginBottom: 40
   }
 });
